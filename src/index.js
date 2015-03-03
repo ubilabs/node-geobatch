33da3c9da45b9cee4ed149817d500d1619e5acb6
Robert Katzki <katzki@ubilabs.net>
2015-03-03 11:44:36 +0100

    feat(index): accept cacheFile param

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
+/* eslint-disable one-var */
+
+const Cache = require('./cache');
+
 /**
  * GeoBatch instance
  * @type {Function}
+ * @param {Object} options The options for the GeoBatch
  */
-const GeoBatch = function() {
+const GeoBatch = function(options) {
+  options = options || {};
 
+  const cacheFile = options.cacheFile || 'geocache.db';
+
+  this.cache = new Cache(cacheFile);
 };
 
 module.exports = GeoBatch;
--- test/index-test.js
+++ test/index-test.js
@@ -1,12 +1,38 @@
+/* eslint-disable no-unused-expressions */
 require('traceur-runner');
 
 var should = require('should'),
+  fs = require('fs'),
   GeoBatch = require('../src/index.js');
 
 describe('Testing index', function() {
+  afterEach(function(done) {
+    fs.exists('geocache.db', function(exists) {
+      if (exists) {
+        fs.unlinkSync('geocache.db');
+      }
+
+      done();
+    });
+  });
+
   it('should create a new instance when called without params', function() {
     var geoBatch = new GeoBatch();
 
     should.exist(geoBatch);
+  });
+
+  it('should accept a cachefile name', function(done) {
+    var geoBatch = new GeoBatch({
+      cacheFile: 'myPersonalGeocache.db'
+    });
+
+    should.exist(geoBatch);
+
+    fs.exists('myPersonalGeocache.db', function(exists) {
+      should(exists).be.true;
+      fs.unlinkSync('myPersonalGeocache.db');
+      done();
+    });
   });
 });

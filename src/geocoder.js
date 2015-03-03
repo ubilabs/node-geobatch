e392d6e37fbdebb6bbbeb4a5b2dd9a12ff529429
Robert Katzki <katzki@ubilabs.net>
2015-03-03 15:46:43 +0100

    feat(geocoder): geocode an address

--- src/geocoder.js
+++ src/geocoder.js
@@ -1,5 +1,3 @@
-/* eslint-disable one-var */
-
 /**
  * Geocoder instance
  * @type {Function}
@@ -7,6 +5,23 @@
  */
 const Geocoder = function(options) {
   options = options || {};
+
+  this.googlemaps = require('googlemaps');
+};
+
+/**
+ * Geocode a single address
+ * @param {String} address The address to geocode
+ * @return {Promise} A new promise
+ */
+Geocoder.prototype.geocodeAddress = function(address) {
+  address = address.replace('\'', '');
+
+  return new Promise((resolve) => {
+    this.googlemaps.geocode(address, (error, response) => {
+      resolve(response.results[0].geometry.location);
+    });
+  });
 };
 
 module.exports = Geocoder;
--- test/geocoder-test.js
+++ test/geocoder-test.js
@@ -10,4 +10,22 @@ describe('Testing geocoder', function() {
 
     should.exist(geocoder);
   });
+
+  it('should geocode an address', function(done) {
+    var geocoder = new Geocoder(),
+      address = 'Juliusstraße 25, 22769 Hamburg',
+      geocode = geocoder.geocodeAddress(address);
+
+    should(geocode).be.fulfilled;
+
+    geocode.then(function(location) {
+      should(location).be.an.object;
+      should(location).have.keys('lat', 'lng');
+      should(location.lat).be.above(53);
+      should(location.lat).be.below(54);
+      should(location.lng).be.above(9);
+      should(location.lng).be.below(10);
+      done();
+    });
+  });
 });

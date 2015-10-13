a4be27a7bf4f27ec88f2d9c9723e45b208bc0bd3
Patrick Mast <mast@ubilabs.net>
2015-10-13 11:58:47 +0200

    doc: add example

---
+++ example/index.js
@@ -0,0 +1,17 @@
+import Geobatch from './src/index';
+
+const accessor = input => input.address,
+  gb = new Geobatch({}, accessor),
+  input = [
+    {
+      address: 'Hamburg'
+    },
+    {
+      address: 'Berlin'
+    }
+  ],
+  s = gb.geocode(input);
+
+s.on('data', result => {
+  console.log(result);
+});
